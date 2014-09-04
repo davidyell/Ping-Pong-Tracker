6fd6d196ad56b2f38b9ca8b9564202067aaa5b12
David Yell <dyell@ukwebmedia.com>
2014-09-04 12:22:32 +0100

    Updated the schema file

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -9,7 +9,7 @@ class AppSchema extends CakeSchema {
 	}
 
 	public $departments = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
 		'name' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
@@ -20,7 +20,7 @@ class AppSchema extends CakeSchema {
 	);
 
 	public $match_types = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
 		'name' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
@@ -31,12 +31,9 @@ class AppSchema extends CakeSchema {
 	);
 
 	public $matches = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'match_type_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'match_type_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'notes' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'tournament_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-		'tournament_round' => array('type' => 'integer', 'null' => false, 'default' => '1'),
-		'tournament_match_num' => array('type' => 'integer', 'null' => false, 'default' => '0'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
@@ -46,11 +43,11 @@ class AppSchema extends CakeSchema {
 	);
 
 	public $matches_players = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'match_id' => array('type' => 'integer', 'null' => false, 'default' => null),
-		'player_id' => array('type' => 'integer', 'null' => false, 'default' => null),
-		'score' => array('type' => 'integer', 'null' => true, 'default' => '-1'),
-		'result' => array('type' => 'string', 'null' => true, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'match_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'player_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'score' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'result' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
 		'indexes' => array(
@@ -60,14 +57,14 @@ class AppSchema extends CakeSchema {
 	);
 
 	public $players = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
 		'first_name' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'nickname' => array('type' => 'string', 'null' => true, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'last_name' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'email' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'facebook_id' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'performance_rating' => array('type' => 'integer', 'null' => false, 'default' => '1000'),
-		'department_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'performance_rating' => array('type' => 'integer', 'null' => false, 'default' => '1000', 'unsigned' => false),
+		'department_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
@@ -77,11 +74,11 @@ class AppSchema extends CakeSchema {
 	);
 
 	public $ratings = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'player_id' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'rating' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'change' => array('type' => 'float', 'null' => false, 'default' => '0', 'length' => 10),
-		'match_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'player_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'rating' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+		'change' => array('type' => 'decimal', 'null' => false, 'default' => '0', 'length' => 10, 'unsigned' => false),
+		'match_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
@@ -91,7 +88,7 @@ class AppSchema extends CakeSchema {
 	);
 
 	public $tournaments = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
 		'name' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'competitors' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
